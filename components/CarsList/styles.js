f6e338174e9efdcf76e594020c53bf101e980c41
Pragyan17 <pragyanpoudel99@gmail.com>
2022-01-15 18:23:45 +0545

    All code committed

---
+++ components/CarsList/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const styles = StyleSheet.create({
+    container:{
+        width:'100%',
+        height: Dimensions.get('window').height,
+        top:20,
+    }
+    
+});
+
+export default styles;
